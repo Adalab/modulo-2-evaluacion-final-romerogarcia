64bccc3942b12d7ecb9231860b428e6932920d30
Noelia R <noeliaromga@gmail.com>
2022-04-10 14:39:04 +0200

    npm run docs, new changes for last details in styles, reset button function commented out because it doesn't run

--- src/js/main.js
+++ src/js/main.js
@@ -1,39 +1,3 @@
-//---------PASOS A REALIZAR BUSCADOR------
-//1. Obtener los datos del HTML que vamos a usar x
-//2. Recoger los cocteles del servidor (fetch) x
-//3. Guardar la información de los cocteles x
-//4. Pintar estos cocteles en el html x
-//5. Usuaria escribe en el input el nombre del coctel  x
-//6. Click en el botón buscar (se genera un evento click) x
-//7. Aparece el coctel que la uausaria a escrito x
-
-
-
-//----------vamos a necesitar de eventos
-//-evento click x
-
-//----------funciones para: 
-//- fetch x
-//- escuchar los cocteles
-//- pintar en el HTML x
-//- handler x 
-//- filtrar los cocteles x
-//- cuando no hay img x
-
-
-//----------PASOS A REALIZAR FAVORITOS-------
-//Una vez ya tenemos los resultados de la búsqueda
-//1. Crear el listado de los favoritos x
-//2. Escuchar cuando la usuaria hace click en un coctel x
-//3. Obtener a que coctel le dio click x
-//4. Existe en el listado de favoritos 
-//5. Si existe lo elimino del listado de favoritos
-//6. si no existe la agregamos al listado de favoritos
-//7. cambiar/añadir una clase para que cuando se seleccione cambie el color/fuente
-
-//-------funcione para:
-//evento click
-
 
 'use strcit';
 //Constantes elementos que necesitamos
@@ -46,7 +10,7 @@ let listCocktails = [];
 let listFavorite = [];
 
 
-//evento que queremos que ejecute
+//evento que queremos que ejecute cuando hacemos click en el botón buscar
 btnShare.addEventListener("click", handlerCocktails);
 
 
@@ -136,21 +100,49 @@ function getApi() {
 
 //función para crear una img cuando no contiene img
 function newImg (drink) {
-  //(if)si strDrinkThumb no tiene mostramos una imagen predeterminada === null
   if(drink.strDrinkThumb === "") {
    return 
    "https://via.placeholder.com/210x295/a2deaf/666666/?text=Drink";
   } else {
     return drink.strDrinkThumb;
   }
-  //(else) al contrario mostramos img de strDrinkThumb 
 }
+
+
+////////////////reset///////////////////
+/*function removeFavs() {
+  document.getElementById('js-favorite').reset();
+  console.log('dando reset');
+}*/
+
+
+//función que recoge los datos de fav
+/*function RemoveClick () {
+  console.log('holi');
+  renderFavorites();//llamo a la función que tiene los datos de la lista de favoritos
+}
+
+//función resetear los datos de la lista de favoritos
+function resetFavotites() {
+  const reset = document.querySelector('.js-btn-reset');
+  console.log('hago click reset');
+  for(const resetFav of reset ) {
+    resetFav.addEventListener("click", RemoveClick)
+  }
+}*/
+
+
+
+
+
 
 //función jefa para ejecutar las demás funciones 
 function handlerCocktails(event) {
   event.preventDefault();
   getApi();
   filteredCocktail(listCocktails);
+  //resetFavorites();
+  //removeFavs();
 }
 
 
